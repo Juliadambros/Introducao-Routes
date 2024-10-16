2ca35185d3dc6148332d47126d35929ada9afcd9
Juliadambros <138934065+Juliadambros@users.noreply.github.com>
2024-10-16 20:56:43 -0300

    IndexE.js
    Erro

---
+++ IndexE.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Erro(){
+    return(
+        <div>
+            <h1><center>ERR0 página não encontrada</center></h1>
+        </div>
+    );
+}
+
+export default Erro;
